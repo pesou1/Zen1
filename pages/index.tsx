d3cefa90368e4ee3165f3594e8de401ef2762b6c
pesou1 <eljaneee24@gmail.com>
2026-04-23 13:40:45 +0800

    Add homepage with featured streams

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Featured Streams</h1>
+            {/* TODO: Add components for featured streams */}
+        </div>
+    );
+};
+
+export default HomePage;
